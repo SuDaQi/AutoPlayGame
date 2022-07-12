8b6e8dfe5dcba4df5d3970e5e2c523de5695d53f
SuDaQi <105043587+SuDaQi@users.noreply.github.com>
2022-07-12 17:22:57 +0800

    test

--- AiPcUser/Main.py
+++ AiPcUser/Main.py
@@ -4,6 +4,16 @@ import xlrd
 import pyperclip
 import random
 
+file = '1.xls'
+# 打开文件
+wb = xlrd.open_workbook(filename=file)
+# 通过索引获取表格sheet页
+sheet1 = wb.sheet_by_index(0)
+imgList = str(sheet1.row(1)[1].value).split('-')
+a = pyautogui.locateCenterOnScreen(imgList[1], confidence=0.9)
+print(a)
+
+
 #鼠标点击事件
 def mouseClick(img):
     clickTimes=0.5
@@ -20,10 +30,10 @@ def mouseClick(img):
 
 
 #遍历图片并定位----------------
-flie = "1.png"
 img=('111.jpg-222.jpg-333.jpg')
 imgList=list(str(img).split('-'))
-print(imgList)
+#a = pyautogui.locateCenterOnScreen(r'C:\Users\ycwb0484\Desktop\waterRPA\', confidence=0.9)
+#print(a)
 def TotalLevelSeek(imgList):
     i=0
     while i<len(imgList):
