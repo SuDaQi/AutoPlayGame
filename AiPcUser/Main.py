d05915c01176ddc75fc8bcac6ef8fe53fd384994
SuDaQi <105043587+SuDaQi@users.noreply.github.com>
2022-07-13 16:38:55 +0800

    Update Main.py

--- AiPcUser/Main.py
+++ AiPcUser/Main.py
@@ -6,15 +6,27 @@ import random
 
 file = '1.xls'
 # 打开文件
-wb = xlrd.open_workbook(filename=file)
+OpenXls = xlrd.open_workbook(filename=file)
 # 通过索引获取表格sheet页
-Level=1
-sheet1 = wb.sheet_by_index(0)
-img=sheet1.row(Level)[1].value
-imgList = str(sheet1.row(Level)[1].value).split('-')
-a = pyautogui.locateCenterOnScreen(imgList[1], confidence=0.9)
+sheet1 = OpenXls.sheet_by_index(0)
 
+#-----------------------
+#逻辑定位
 
+def SoloLevelSeek(sheet1,Level):
+    #读取图片列表
+    imgList = list(str(sheet1.row(Level)[1].value).split('-'))
+    i = 0
+    while i < len(imgList):
+        img = pyautogui.locateCenterOnScreen(imgList[i], confidence=0.9)
+        if img is not None:
+            print(f"捕捉到当前游戏的进度,{Level}级第{i+1}步.{img}")
+
+        i += 1
+        if i == len(imgList)-1 and img is None:
+            print(f"没有监测到游戏窗口,停止在{Level}级第{i+1}步.")
+
+SoloLevelSeek(sheet1,1)
 
 #鼠标点击事件
 def mouseClick(img):
@@ -31,32 +43,9 @@ def mouseClick(img):
     time.sleep(0.5)
 
 
-#遍历图片并定位----------------
 
 
-
-
-#a = pyautogui.locateCenterOnScreen(r'C:\Users\ycwb0484\Desktop\waterRPA\', confidence=0.9)
-#print(a)
-
-
-#-----------------------
-#逻辑定位
-
-def SoloLevelSeek(img,Level):
-    imgList = list(str(img).split('-'))
-    i = 0
-    while i < len(imgList):
-        img = pyautogui.locateCenterOnScreen(imgList[i], confidence=0.9)
-        if img is not None:
-            print(f"捕捉到当前游戏的进度,{Level}级第{i+1}步.{img}")
-
-        i += 1
-        if i == len(imgList)-1 and img is None:
-            print(f"没有监测到游戏窗口,停止在{Level}级第{i+1}步.")
-
-SoloLevelSeek(img,1)
-def LocationLevelSeek(imgNumLocation):
+def TotalLevelSeek(imgNumLocation):
 
     pass
 
